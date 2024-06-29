823dbbe6bbad1ec7f11e71bbccb39cf3507f3f0b
Tomasz Kiljańczyk <kiljanczyk@deltologic.com>
2024-06-29 16:48:42 +0200

    Add genes redux slice

--- src/common/redux/slices/settings/settings.ts
+++ src/common/redux/slices/settings/settings.ts
@@ -3,44 +3,23 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { createAppSlice } from '../../redux-common';
 
 import diagnoses from './diagnoses';
+import genes from './genes';
 import illnesses from './illnesses';
 import mutations from './mutations';
 import recommendations from './recommendations';
 import staff from './staff';
 import testMethods from './test-methods';
 
-type RecommendationId = string;
-type MutationId = string;
-
-interface Genes {
-    id: string;
-    name: string;
-    testMethods: string[];
-    mutationIds: MutationId[];
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface Illness {
-    id: string;
-    name: string;
-    recommendationIds: RecommendationId[];
-}
-
-interface SettingsState {
-    genes: Genes[];
-}
-
 const settingsSlice = createAppSlice({
     name: 'settings',
-    initialState: {
-        genes: []
-    } as SettingsState,
+    initialState: {},
     reducers: {}
 });
 
 const settingsReducer = combineReducers({
     [settingsSlice.name]: settingsSlice.reducer,
     [diagnoses.name]: diagnoses.reducer,
+    [genes.name]: genes.reducer,
     [mutations.name]: mutations.reducer,
     [illnesses.name]: illnesses.reducer,
     [recommendations.name]: recommendations.reducer,
