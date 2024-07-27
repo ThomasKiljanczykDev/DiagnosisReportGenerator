408a299a15147e0fbc4583de0ff9908514dad6bc
Tomasz Kiljańczyk <kiljanczyk@deltologic.com>
2024-07-27 13:02:41 +0200

    Fix linter errors

--- js/react-app/src/hooks/redux.ts
+++ js/react-app/src/hooks/redux.ts
@@ -1,8 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-import { type RootState } from '@/redux/store';
-import { type AppDispatch } from '@/redux/store';
+import { type AppDispatch, type RootState } from '@/redux/store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
