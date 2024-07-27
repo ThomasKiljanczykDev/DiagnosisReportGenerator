8ff966fa2aa72f19329cd1dfc671abeed7d70302
Tomasz Kiljańczyk <kiljanczyk@deltologic.com>
2024-07-27 14:09:09 +0200

    Add additional import in electron-app index.tsx

--- js/electron-app/src/renderer/index.tsx
+++ js/electron-app/src/renderer/index.tsx
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom/client';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import App from '@diagnosis-report-generator/react-app/App'
-import Index from '@diagnosis-report-generator/react-app/index'
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Index />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
 
 postMessage({ payload: 'removeLoading' }, '*');
