eef7e11e6761147b3056fe5aaf51e68b73602e8a
Tomasz Kiljańczyk <kiljanczyk@deltologic.com>
2024-07-29 00:22:23 +0200

    Add test projects

---
+++ dotnet/test/Gunock.DiagnosisReportGenerator.Application.Tests/DiagnosisReportGeneratorApplicationTestModule.cs
@@ -0,0 +1,9 @@
+﻿using Gunock.DiagnosisReportGenerator.Domain.Tests;
+using Volo.Abp.Modularity;
+
+namespace Gunock.DiagnosisReportGenerator.Application.Tests;
+
+[DependsOn(typeof(DiagnosisReportGeneratorApplicationModule), typeof(DiagnosisReportGeneratorDomainTestModule))]
+public class DiagnosisReportGeneratorApplicationTestModule : AbpModule
+{
+}
---
+++ dotnet/test/Gunock.DiagnosisReportGenerator.EntityFrameworkCore.Tests/EntityFrameworkCore/DiagnosisReportGeneratorEntityFrameworkCoreTestModule.cs
@@ -0,0 +1,64 @@
+﻿using Gunock.DiagnosisReportGenerator.Application.Tests;
+using Gunock.DiagnosisReportGenerator.EntityFrameworkCore.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.Sqlite;
+using Volo.Abp.Modularity;
+using Volo.Abp.Uow;
+
+namespace Gunock.DiagnosisReportGenerator.EntityFrameworkCore.Tests.EntityFrameworkCore;
+
+[DependsOn(
+    typeof(DiagnosisReportGeneratorApplicationTestModule),
+    typeof(DiagnosisReportGeneratorEntityFrameworkCoreModule),
+    typeof(AbpEntityFrameworkCoreSqliteModule)
+)]
+public class DiagnosisReportGeneratorEntityFrameworkCoreTestModule : AbpModule
+{
+    private SqliteConnection? _sqliteConnection;
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddAlwaysDisableUnitOfWorkTransaction();
+
+        ConfigureInMemorySqlite(context.Services);
+    }
+
+    private void ConfigureInMemorySqlite(IServiceCollection services)
+    {
+        _sqliteConnection = CreateDatabaseAndGetConnection();
+
+        services.Configure<AbpDbContextOptions>(
+            options =>
+            {
+                options.PreConfigure<DiagnosisReportGeneratorDbContext>(
+                    configurationContext => { configurationContext.DbContextOptions.UseLazyLoadingProxies(); }
+                );
+                options.Configure(context => { context.DbContextOptions.UseSqlite(_sqliteConnection); });
+            }
+        );
+    }
+
+    public override void OnApplicationShutdown(ApplicationShutdownContext context)
+    {
+        _sqliteConnection?.Dispose();
+    }
+
+    private static SqliteConnection CreateDatabaseAndGetConnection()
+    {
+        var connection = new AbpUnitTestSqliteConnection("Data Source=:memory:");
+        connection.Open();
+
+        var options = new DbContextOptionsBuilder<DiagnosisReportGeneratorDbContext>().UseSqlite(connection).Options;
+
+        using var context = new DiagnosisReportGeneratorDbContext(options);
+        context.GetService<IRelationalDatabaseCreator>().CreateTables();
+
+        return connection;
+    }
+}
