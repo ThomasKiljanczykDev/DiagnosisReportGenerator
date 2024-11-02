72bf37d1651cfd0f89a668bd4440894af4faf0ed
Tomasz Kiljańczyk <kiljanczyk@deltologic.com>
2024-11-02 22:24:38 +0100

    Remove MainAppBar

--- js/electron-app/src/modules/core/components/AppPageContent.tsx
+++ js/electron-app/src/modules/core/components/AppPageContent.tsx
@@ -1,10 +1,66 @@
 import React, { type PropsWithChildren, type ReactNode, memo } from 'react';
 
-import { Grid2 } from '@mui/material';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded';
+import { Breadcrumbs, Grid2 } from '@mui/material';
+import { Link, useLocation } from '@tanstack/react-router';
 
 interface AppPageContentProps {
     title: string;
     actionButtons?: ReactNode;
+}
+
+function AppBreadcrumbs() {
+    const location = useLocation();
+
+    const pathElements = location.pathname.split('/');
+
+    return (
+        <Breadcrumbs
+            separator={<NavigateNextRoundedIcon fontSize="small" />}
+            aria-label="breadcrumb"
+            sx={{
+                color: 'black'
+            }}
+        >
+            {pathElements.map((pathElement, index) => {
+                const isRoot = index === 0;
+                const to = pathElements.slice(0, index + 1).join('/');
+
+                // TODO: Move styles to css file
+                if (isRoot) {
+                    return (
+                        <Link
+                            key={index}
+                            to="/"
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                color: 'inherit',
+                                textDecoration: 'none'
+                            }}
+                        >
+                            <HomeRoundedIcon />
+                        </Link>
+                    );
+                }
+
+                // TODO: Add i18n translations
+                return (
+                    <Link
+                        key={index}
+                        to={to}
+                        style={{
+                            color: 'inherit',
+                            textDecoration: 'none'
+                        }}
+                    >
+                        {pathElement}
+                    </Link>
+                );
+            })}
+        </Breadcrumbs>
+    );
 }
 
 const AppPageContent = memo(function AppPageContent(props: PropsWithChildren<AppPageContentProps>) {
@@ -19,13 +75,15 @@ const AppPageContent = memo(function AppPageContent(props: PropsWithChildren<App
             minHeight={0}
             maxWidth="100%"
         >
+            <Grid2 flexShrink={0} minWidth={0} minHeight={0} maxWidth="100%">
+                <AppBreadcrumbs />
+            </Grid2>
             <Grid2
                 display="flex"
                 flexShrink={0}
                 minWidth={0}
                 minHeight={0}
                 maxWidth="100%"
-                direction="row"
                 justifyContent="space-between"
             >
                 <h2 style={{ color: 'black', margin: 0 }}>{props.title}</h2>
--- js/electron-app/src/routes/reports/_layout.tsx
+++ js/electron-app/src/routes/reports/_layout.tsx
@@ -3,20 +3,18 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { Outlet, createFileRoute } from '@tanstack/react-router';
 
-import MainAppBar from '@/modules/core/components/MainAppBar';
 import MainDrawer from '@/modules/core/components/drawer/MainDrawer';
 
 export function ReportsLayout() {
     return (
         <>
-            <MainAppBar />
             <Box
                 minWidth={0}
                 minHeight={0}
                 sx={{
                     display: 'flex',
                     width: '100%',
-                    height: `calc(100% - ${64}px)`
+                    height: `100%`
                 }}
             >
                 <MainDrawer />
